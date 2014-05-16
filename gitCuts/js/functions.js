86a2152762a060a7fc51077c0cd6c68892c2461a
Brady <brady@brightroll.com>
2014-05-15 23:07:52 -0700

    all good

--- gitCuts/js/functions.js
+++ gitCuts/js/functions.js
@@ -1,23 +1,24 @@
 
 var gitSnip = $("li pre");
+var words = ["Boom!" , "Whammy!", "Booya!" , "Holler!", "Broskee!" , "All Good!" , "Right on!"];
 
-//Code Clipboard Function
-gitSnip.on("click" , function(){
-	var words = ["Boom!" , "Whammy!", "Booya!" , "Holler!", "Broskee!" , "All Good!" , "Right on!"];
-	var randomWord = words[Math.floor(Math.random() * words.length)];
-	$(".modal strong").text(randomWord);
-}).zclip({
-	path: 'http://www.steamdev.com/zclip/js/ZeroClipboard.swf',
-    copy: function () {
-        return $(this).text().substring(2);
-        return randomWord;
-     },
-     afterCopy:function(){
-     	console.log($(this).text());
-     	$(".overlay").fadeIn().addClass('active');
+// main.js
+ZeroClipboard.config({ forceHandCursor: true });
+var client = new ZeroClipboard( gitSnip, {
+    moviePath: "ZeroClipboard.swf"
+});
 
-     	setTimeout(function(){
-     		$(".overlay").fadeOut().removeClass('active');
-     	},1200);
-    }
+client.on( "load", function(client) {
+   console.log( "movie is loaded" );
+
+  client.on( "complete", function(client, args) {
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+        $(".modal strong").text(randomWord);
+
+        $(".overlay").fadeIn().addClass('active');
+        setTimeout(function(){
+            $(".overlay").fadeOut().removeClass('active');
+        },1200);
+    
+  });
 });
